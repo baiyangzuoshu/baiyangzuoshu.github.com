50bfbf693d6b4c288b0f3718eaf33f35491cad2f
maomao <baiyangzuo_shu@aliyun.com>
2020-11-15 00:21:05 +0800

    Site updated: 2020-11-15 00:21:04

---
+++ images/webp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function    travFolder(){
+    echo "travFolder"
+    flist=`ls $1`
+    cd $1
+    #echo $flist
+    for f in $flist
+    do
+        if test -d $f
+        then
+            #echo "dir:$f"
+            travFolder $f
+        else
+            #echo "file:$f"
+            magick $f ${f}.webp && rm $f
+        fi
+    done
+    cd ../
+}
+
+dir=.
+travFolder $dir
